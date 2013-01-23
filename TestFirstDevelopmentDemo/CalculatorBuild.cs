841d48f14283967e521b6402e5a6ebc3d54c2c82
Chad <Windows Live ID\england3100@live.com>
2013-01-23 11:10:45 -0500

    merge with tfs and github

---
+++ TestFirstDevelopmentDemo/CalculatorBuild.cs
@@ -0,0 +1,12 @@
+﻿namespace TestFirstDevelopmentDemo
+{
+    using System;
+
+    public class CalculatorBuild
+    {
+        public int Calculator(string s)
+        {
+            return 0;
+        }
+    }
+}
