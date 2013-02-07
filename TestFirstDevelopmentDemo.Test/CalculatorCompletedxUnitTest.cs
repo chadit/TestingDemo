8ce6faa0715a43c35d86f12bd93f49d80d5532a6
Chad <chadit@gmail.com>
2013-02-07 10:44:12 -0500

    added giles to demo

---
+++ TestFirstDevelopmentDemo.Test/CalculatorCompletedxUnitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace TestFirstDevelopmentDemo.Test
+{
+    using Xunit;
+
+    public class CalculatorCompletedxUnitTest
+    {
+        // Arrange
+        private readonly CalculatorCompleted cal;
+
+        public CalculatorCompletedxUnitTest()
+        {
+            cal = new CalculatorCompleted();
+        }
+
+        [Fact]
+        public void CalculatorTestEmptyString()
+        {
+            // Act
+            var returnValue = cal.Calculator(string.Empty);
+
+            // Assert
+            Assert.Equal(0, returnValue);
+        }
+
+    }
+}
---
+++ TestFirstDevelopmentDemoxUnit.Test/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFirstDevelopmentDemoxUnit.Test
+{
+    using TestFirstDevelopmentDemo;
+
+    using Xunit;
+
+
+    public class Class1
+    {
+        // Arrange
+        private CalculatorCompleted cal;
+
+        public Class1()
+        {
+            cal = new CalculatorCompleted();
+        }
+
+        [Fact]
+        public void CalculatorTestEmptyString()
+        {
+            // Act
+            var returnValue = cal.Calculator(string.Empty);
+
+            // Assert
+            Assert.Equal(0, returnValue);
+        }
+
+    }
+}
