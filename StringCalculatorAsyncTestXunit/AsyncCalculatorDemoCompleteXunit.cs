26f29f6f97af1e1da7f12b156801b15e5cdf5abe
Chad <chadit@gmail.com>
2013-03-15 22:19:10 -0400

    Added xunit theory example

---
+++ StringCalculatorAsyncTestXunit/AsyncCalculatorDemoCompleteXunit.cs
@@ -0,0 +1,34 @@
+﻿namespace StringCalculatorAsyncTestXunit
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using StringCalculatorAsync;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class AsyncCalculatorDemoCompleteXunit
+    {
+        // Arrange
+        private readonly AsyncCalculatorDemoComplete cal;
+
+        public AsyncCalculatorDemoCompleteXunit()
+        {
+            cal = new AsyncCalculatorDemoComplete();
+        }
+
+        [Theory,
+            InlineData(0, ""),
+            InlineData(3, "1,2"),
+            InlineData(6, "1,2,3"),
+            InlineData(3, @"1\n2")]
+        public async Task AsyncCalculatorEmptyStringTest(int expectedReturn, string passedInValue)
+        {
+            // Act
+            await this.cal.Calculator(passedInValue);
+
+            // Assert
+            Assert.Equal(expectedReturn, cal.CalculatedValue);
+        }
+    }
+}
